68d2f5d73377a23202027d96990cbd3a3f5c08d8
kkzznn0428 <kazumaru.blog@gmail.com>
2020-12-30 20:51:17 +0900

    記事リストの追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
-// src/pages/index.js
 import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
+import PostLink from "../components/post-link"
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
+      <PostLink />
+	  <PostLink />
+	  <PostLink />
     </Layout>
   )
 }
