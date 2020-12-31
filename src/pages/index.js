f755197fdb48db489617ae161d01da91a6cc6d7b
kkzznn0428 <kazumaru.blog@gmail.com>
2020-12-31 16:25:08 +0900

    SEOのためにheadタグを設定

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import Layout from "../components/layout"
 import Hero from "../components/hero"
 import PostLink from "../components/post-link"
 import { graphql } from "gatsby"
+import SEO from "../components/seo"
 
 export default function Home({ data }) {
   return (
     <Layout>
+      <SEO title="カズマルブログ" description="Gatsbyを使って作ったブログです" />
       <Hero />
       {data.allContentfulPost.edges.map(edge =>
         <PostLink key={edge.node.slug} post={edge.node} />
